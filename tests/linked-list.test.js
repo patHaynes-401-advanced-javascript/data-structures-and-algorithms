524f927e92a71fbe748bb9621820f5f99a56d6d5
Pat Haynes <pathaynes32@gmail.com>
2019-09-23 16:18:03 -0700

    added some white space

--- tests/linked-list.test.js
+++ tests/linked-list.test.js
@@ -6,18 +6,21 @@ describe('linked list tests', () => {
     const result = { head: null, size: 0 };
     expect(list).toEqual(result);
   });
+
   it('can properly insert into the linked list', () => {
     const list = new linkListImport.LinkedList();
     expect(list.size).toEqual(0);
     list.insert('testing');
     expect(list.size).toBe(1);
   });
+
   it('the head property will properly point to the first node in the linked list', () => {
     const list = new linkListImport.LinkedList();
     list.insert('testing2');
     const result = { value: 'testing2', next: null };
     expect(list.head).toEqual(result);
   });
+
   it('can insert multiple nodes into the linked list', () => {
     const list = new linkListImport.LinkedList();
     list.insert('testing 1');
@@ -31,17 +34,20 @@ describe('linked list tests', () => {
     expect(list.head.next.next).toEqual(first);
     expect(list.size).toBe(3);
   });
+
   it('will return true when finding a value within the linked list that exists', () => {
     const list = new linkListImport.LinkedList();
     list.insert('test');
     const value = list.head.value;
     expect(list.includes(value)).toBe(true);
   });
+
   it('will return false when searching for a value in the linked list that does not exist', () => {
     const list = new linkListImport.LinkedList();
     list.insert('test');
     expect(list.includes('wrong')).toBe(false);
   });
+  
   it('can properly return a collection of all the values that exist in the linked list', () => {
     const list = new linkListImport.LinkedList();
     list.insert('test1');
