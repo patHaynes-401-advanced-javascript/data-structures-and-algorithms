b4b578f5a9b2d092cbf3a335bdb5c5dba3a30dff
Pat Haynes <pathaynes32@gmail.com>
2019-10-02 16:29:21 -0700

    ok now its working

--- challenges/elevenMerge/eleven-merge.js
+++ challenges/elevenMerge/eleven-merge.js
@@ -5,12 +5,10 @@ function mergeLists(listOne, listTwo) {
   const list = new linkListModel.LinkedList();
   let firstNode = listOne.head;
   let secondNode = listTwo.head;
-  console.log(firstNode);
-  console.log(secondNode);
 
   while(firstNode.next !== null && secondNode.next !== null) {
-    console.log(list.append(firstNode.value));
-    console.log(list.append(secondNode.value));
+    list.append(firstNode.value);
+    list.append(secondNode.value);
     firstNode = firstNode.next;
     secondNode = secondNode.next;
   }
---
+++ tests/stack-and-queue.test.js
@@ -0,0 +1,60 @@
+const stackImport = require('../challenges/stackAndQueue/stack-and-queue');
+
+describe('Stack and Queue', () => {
+
+  it('Can successfully push onto a stack', () => {
+    const stack = new stackImport.Stack();
+    const result = stack.push('element');
+    expect(result.value).toBe('element');
+  });
+
+  it.skip('Can successfully push multiple values onto a stack', () => {
+    const stack = new stackImport.Stack();
+    stack.push('first element');
+    stack.push('second element');
+    expect(stack.top.value).toBe('second element');
+    expect(stack.top.next.value).toBe('first element');
+  });
+
+  it.skip('Can successfully pop off the stack', () => {
+
+  });
+
+  it.skip('Can successfully empty a stack after multiple pops', () => {
+
+  });
+
+  it.skip('Can successfully peek the next item on the stack', () => {
+
+  });
+
+  it.skip('Can successfully instantiate an empty stack', () => {
+
+  });
+
+  it.skip('Can successfully enqueue into a queue', () => {
+
+  });
+
+  it.skip('Can successfully enqueue multiple values into a queue', () => {
+
+  });
+
+  it.skip('Can successfully dequeue out of a queue the expected value', () => {
+
+  });
+
+  it.skip('Can successfully peek into a queue, seeing the expected value', () => {
+
+  });
+
+  it.skip('Can successfully empty a queue after multiple dequeues', () => {
+
+  });
+
+  it.skip('Can successfully instantiate an empty queue', () => {
+
+  });
+
+});
+
