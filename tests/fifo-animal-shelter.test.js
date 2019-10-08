889171c0f79fee1d6efee15b227947e1c4607df0
Pat Haynes <pathaynes32@gmail.com>
2019-10-07 21:45:53 -0700

    getting started

---
+++ tests/fifo-animal-shelter.test.js
@@ -0,0 +1,15 @@
+describe('linked list tests', () => {
+
+
+  it('dequeues based on pref', () => {
+
+  });
+
+  it('returns an error if you try to remove more than are available', () => {
+
+  });
+
+
+
+
+});
