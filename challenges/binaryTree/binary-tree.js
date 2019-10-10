e0dc3739d4459c6bba7fcdb6a326797c16f8055a
Pat Haynes <pathaynes32@gmail.com>
2019-10-09 22:42:47 -0700

    getting tests set up

--- challenges/binaryTree/binary-tree.js
+++ challenges/binaryTree/binary-tree.js
@@ -11,7 +11,6 @@ class BinaryTree {
     this.root = null;
   }
 
-
   preOrder(node) {
     if(node === null) return [];
 
@@ -64,3 +63,44 @@ class BinaryTree {
     return result;
   }
 }
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  add(value) {
+    let node = this.root;
+    return recurse(value, node);
+  }
+
+  contains(value) {
+    let node = this.root;
+    return recurseFind(value, node);
+  }
+
+}
+
+function recurse(value, node) {
+  let dir;
+  value > node.value ? dir = 'right' : dir = 'left';
+  if(node[dir]) {
+    return recurse(value, node[dir]);
+  }
+  return node[dir] = new Node(value);
+}
+
+function recurseFind(value, node) {
+  let dir;
+  if(value === node.value) return true;
+  value > node.value ? dir = 'right' : dir = 'left';
+  if(node[dir]) return recurseFind(value, node[dir]);
+  else return false;
+
+}
+
+module.exports = {
+  Node,
+  BinaryTree,
+  BinarySearchTree
+};
---
+++ tests/binary-tree.test.js
@@ -0,0 +1,35 @@
+const { Node, BinarySearchTree, BinaryTree } = require('../challenges/binaryTree/binary-tree');
+
+describe('tree testing', () => {
+
+  it('can successfully make an empty ree with a single root', () => {
+
+  });
+
+  it('can successfully make a tree with right and left child', () => {
+
+  });
+
+  it('can successfully return a collection from a preorder traversal', () => {
+
+  });
+
+  it('can successfully return a collection from an inorder traversal', () => {
+
+  });
+
+  it('can successfully return a collection from a postorder traversal', () => {
+
+  });
+
+  it('can successfully add a node with a certain value to where it is supposed to go', () => {
+
+  });
+
+  it('can successfully locate a node with the value provided', () => {
+
+  });
+
+
+
+});
