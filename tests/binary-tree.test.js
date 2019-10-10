ce99de6e430728bc4c0eee393c1686ffb2c99e81
Pat Haynes <pathaynes32@gmail.com>
2019-10-09 22:48:15 -0700

    first three tests down

--- tests/binary-tree.test.js
+++ tests/binary-tree.test.js
@@ -1,32 +1,41 @@
-const { Node, BinarySearchTree, BinaryTree } = require('../challenges/binaryTree/binary-tree');
+const { Node, BinaryTree } = require('../challenges/binaryTree/binary-tree');
 
 describe('tree testing', () => {
 
   it('can successfully make an empty ree with a single root', () => {
-
+    const tree = new BinaryTree();
+    expect(tree.root).toBe(null);
   });
 
   it('can successfully make a tree with right and left child', () => {
-
+    const tree = new BinaryTree();
+    tree.root = new Node('root');
+    expect(tree.root.value).toBe('root');
   });
 
   it('can successfully return a collection from a preorder traversal', () => {
+    const tree = new BinaryTree();
+    tree.root = new Node('root');
+    tree.root.left = new Node('left');
+    tree.root.right = new Node('right');
+    expect(tree.root.value).toBe('root');
+    expect(tree.root.left.value).toBe('left');
+    expect(tree.root.right.value).toBe('right');
+  });
+
+  it.skip('can successfully return a collection from an inorder traversal', () => {
 
   });
 
-  it('can successfully return a collection from an inorder traversal', () => {
+  it.skip('can successfully return a collection from a postorder traversal', () => {
 
   });
 
-  it('can successfully return a collection from a postorder traversal', () => {
+  it.skip('can successfully add a node with a certain value to where it is supposed to go', () => {
 
   });
 
-  it('can successfully add a node with a certain value to where it is supposed to go', () => {
-
-  });
-
-  it('can successfully locate a node with the value provided', () => {
+  it.skip('can successfully locate a node with the value provided', () => {
 
   });
 
